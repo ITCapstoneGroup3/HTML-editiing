eb036e3c0495ef015be5587aa3b63d4b3491f654
Zak Smith <notfuzzeh@gmail.com>
2022-11-01 23:13:47 -0500

    test

--- form.php
+++ form.php
@@ -3,156 +3,47 @@
 <head>  
 <meta charset="UTF-8">
     <!---<title> Responsive Registration Form | CodingLab </title>--->
-    <link rel="stylesheet" href="style.css">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Registration Form</title>  
+    <link rel="stylesheet" href="main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>  
 <meta name="viewport" content="width=device-width, initial-scale=1">  
-<style>
-body{
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 10px;
-  background: #57DDFE;
-}
-.container{
-  max-width: 700px;
-  width: 100%;
-  background-color: #fff;
-  padding: 25px 30px;
-  border-radius: 5px;
-  box-shadow: 0 5px 10px rgba(0,0,0,0.15);
-}
-.container .title{
-  font-size: 25px;
-  font-weight: 500;
-  position: relative;
-}
-.container .title::before{
-  content: "";
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  height: 3px;
-  width: 30px;
-  border-radius: 5px;
-  background: linear-gradient(135deg, #71b7e6, #9b59b6);
-}
-.content form .user-details{
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin: 20px 0 12px 0;
-}
-form .user-details .input-box{
-  margin-bottom: 15px;
-  width: calc(100% / 2 - 20px);
-}
-form .input-box span.details{
-  display: block;
-  font-weight: 500;
-  margin-bottom: 5px;
-}
-.user-details .input-box input{
-  height: 45px;
-  width: 100%;
-  outline: none;
-  font-size: 16px;
-  border-radius: 5px;
-  padding-left: 15px;
-  border: 1px solid #ccc;
-  border-bottom-width: 2px;
-  transition: all 0.3s ease;
-}
-.user-details .input-box input:focus,
-.user-details .input-box input:valid{
-  border-color: #57DDFE;
-}
- form .gender-details .gender-title{
-  font-size: 20px;
-  font-weight: 500;
- }
- form .category{
-   display: flex;
-   width: 80%;
-   margin: 14px 0 ;
-   justify-content: space-between;
- }
- form .category label{
-   display: flex;
-   align-items: center;
-   cursor: pointer;
- }
- form .category label .dot{
-  height: 18px;
-  width: 18px;
-  border-radius: 50%;
-  margin-right: 10px;
-  background: #d9d9d9;
-  border: 5px solid transparent;
-  transition: all 0.3s ease;
-}
- #dot-1:checked ~ .category label .one,
- #dot-2:checked ~ .category label .two,
- #dot-3:checked ~ .category label .three{
-   background: #57DDFE;
-   border-color: #57DDFE;
- }
- form input[type="radio"]{
-   display: none;
- }
- form .button{
-   height: 45px;
-   margin: 35px 0
- }
- form .button input{
-   height: 100%;
-   width: 100%;
-   border-radius: 5px;
-   border: none;
-   color: #fff;
-   font-size: 18px;
-   font-weight: 500;
-   letter-spacing: 1px;
-   cursor: pointer;
-   transition: all 0.3s ease;
-   background: #098D9C;
- }
- form .button input:hover{
-  
-  background: #57DDFE;
-  }
- @media(max-width: 584px){
- .container{
-  max-width: 100%;
-}
-form .user-details .input-box{
-    margin-bottom: 15px;
-    width: 100%;
-  }
-  form .category{
-    width: 100%;
-  }
-  .content form .user-details{
-    max-height: 300px;
-    overflow-y: scroll;
-  }
-  .user-details::-webkit-scrollbar{
-    width: 5px;
-  }
-  }
-  @media(max-width: 459px){
-  .container .content .category{
-    flex-direction: column;
-  }
-}
-</style>
 
 </head>  
 <body>  
 
+  <?php
+  $conn = mysqli_connect("dbuno.culbmfqkukyt.us-east-1.rds.amazonaws.com", "admin", "ITgroup3!", "dbuno");
+   
+  // Check connection
+  if($conn === false){
+      die("ERROR: Could not connect. "
+          . mysqli_connect_error());
+  }
+   
+  // Taking all 5 values from the form data(input)
+  $first_name =  $_REQUEST['first_name'];
+  $last_name = $_REQUEST['last_name'];
+  $email = $_REQUEST['email'];
+  $user_name = $_REQUEST['user_name'];
+   
+  // Performing insert query execution
+  // here our table name is college
+  $sql = "INSERT INTO college  VALUES ('$first_name',
+      '$last_name','$gender','$address','$email')";
+   
+  if(mysqli_query($conn, $sql)){
+      echo "<h3>Account created</h3>";
 
+      echo nl2br("\n$first_name\n $last_name\n "
+          . "$user_name\n $email");
+  } else{
+      echo "ERROR: Hush! Sorry $sql. "
+          . mysqli_error($conn);
+  }
+   
+  // Close connection
+  mysqli_close($conn);
+  ?>
 
 <div class="container">
     <div class="title">Create an account</div>
